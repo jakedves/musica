b72f14a880313fc5fbc3f077e4fe465af2955473
Jake <jakedves@gmail.com>
2022-06-19 03:28:15 +0100

    blueprint for the user data class

---
+++ musica/MusicaUser.swift
@@ -0,0 +1,18 @@
+//
+//  MusicaUser.swift
+//  musica
+//
+//  Created by Jake Davies on 19/06/2022.
+//
+
+import Foundation
+
+struct MusicaUser: Identifiable, Equatable {
+    var id: UUID
+    var name: String
+    var bio: String
+    var dob: Date
+    var badges: [String]?
+    var videos: [Data]
+    var highlights: [UUID] // each video has a video ID, and the highlights are pointers to those videos
+}
