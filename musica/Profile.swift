5d697366d967a33ba3e85aa7df978cef499d0b7e
Jake <jakedves@gmail.com>
2022-06-19 03:16:54 +0100

    profile page skeleton

---
+++ musica/Profile.swift
@@ -0,0 +1,73 @@
+//
+//  Profile.swift
+//  musica
+//
+//  Created by Jake Davies on 19/06/2022.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        ZStack {
+            Color.purple.ignoresSafeArea()
+            
+            VStack {
+                profilePicture
+                    .frame(width: Constants.photoRadius,
+                           height: Constants.photoRadius)
+                name
+                bio
+                tags
+                highlights
+                allClipsButton
+                Spacer()
+                
+            }
+            .padding()
+        }
+    }
+    
+    private var name: some View {
+        Text("Musician").bold()
+    }
+    
+    private var profilePicture: some View {
+        Circle().fill(Color.white)
+    }
+    
+    private var bio: some View {
+        Text("This is my bio.").frame(width: Constants.bioWidth)
+    }
+    
+    // TODO: Replace with a TagList view
+    private var tags: some View {
+        Rectangle().frame(width: 200.0, height: 50.0)
+    }
+    
+    // TODO: Replace with a ProfileHighlights view
+    private var highlights: some View {
+        HStack {
+            Rectangle().aspectRatio(9 / 16, contentMode: .fit)
+            Rectangle().aspectRatio(9 / 16, contentMode: .fit)
+            Rectangle().aspectRatio(9 / 16, contentMode: .fit)
+        }
+    }
+    
+    private var allClipsButton: some View {
+        Button("All clips") { }
+            .frame(width: 100.0)
+            .background(Color.white)
+    }
+    
+    private struct Constants {
+        static let photoRadius: CGFloat = 130.0
+        static let bioWidth: CGFloat = 230.0
+    }
+}
+
+struct Profile_Previews: PreviewProvider {
+    static var previews: some View {
+        Profile()
+    }
+}
