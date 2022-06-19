5afaa1e35b10239b5e111d1524ff9ea3271266b3
Jake <jakedves@gmail.com>
2022-06-19 02:19:19 +0100

    Initial Commit

---
+++ musica/musicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicaApp.swift
+//  musica
+//
+//  Created by Jake Davies on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct musicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
