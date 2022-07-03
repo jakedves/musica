cb1e5e5f0b8e5c24a0d23843252f439d646cbce2
Jake <jakedves@gmail.com>
2022-07-03 19:59:46 +0100

    Dummy data added

--- musica/Screens/Profile.swift
+++ musica/Screens/Profile.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct Profile: View {
+    let verified = true
+    let name = "Jake"
+    let tags = [Tag("rock", .red), Tag("pop", .purple), Tag("jazz", .orange),
+                Tag("guitar", .red), Tag("metal", .black)]
+    
     var body: some View {
         ZStack {
             VStack {
                 profilePicture
                     .frame(width: Constants.photoRadius,
                            height: Constants.photoRadius)
-                name
+                HStack(spacing: 2.0) {
+                    nameView
+                    Image(systemName: verified ? Constants.verifiedIcon : "")
+                        .foregroundColor(.yellow)
+                }
+                .font(.title3)
                 bio
-                tags
+                tagList
                 highlights
                 allClipsButton
                 songBookButton
@@ -27,26 +37,30 @@ struct Profile: View {
         }
     }
     
-    private var name: some View {
-        Text("Musician").bold()
+    private var nameView: some View {
+        Text(name).bold()
     }
     
     private var profilePicture: some View {
-        Circle().fill(Color.blue)
+        Image("Test")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .clipShape(Circle())
+        //Circle().fill(Color.blue)
     }
     
     private var bio: some View {
         Text("This is my bio.").frame(width: Constants.bioWidth)
     }
     
-    private var tags: some View {
-        TagList()
+    private var tagList: some View {
+        TagList(tags)
     }
     
     // TODO: Replace with a ProfileHighlights view
     private var highlights: some View {
         LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
-            // ForEach (user.highlights) { highlight in HighlightView(highlight) } 
+            // ForEach (user.highlights) { highlight in HighlightView(highlight) }
             Button {
             } label: {
                 Highlight()
@@ -64,16 +78,17 @@ struct Profile: View {
     }
     
     private var allClipsButton: some View {
-        Button("All clips") { }.musicaLargeButton(.blue)
+        Button { } label: { Text("All Clips").musicaLargeButton(.blue) }
     }
     
     private var songBookButton: some View {
-        Button("Musician's Songbook") { }.musicaLargeButton(.blue)
+        Button {} label: { Text("\(name)'s Songbook").musicaLargeButton(.blue) }
     }
     
     private struct Constants {
         static let photoRadius: CGFloat = 130.0
         static let bioWidth: CGFloat = 230.0
+        static let verifiedIcon = "star.fill"
     }
 }
 
